0de6757d16d1255f8a3eadce99bf688d89a824cb
ksetherg <ksetherg@gmail.com>
2022-07-24 10:10:24 +0200

    fix

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from typing import Dict, List, Any
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from web3 import Web3, HTTPProvider
+from structlog import get_logger
+
+logger = get_logger()
 
 
 web3_provider = Web3(HTTPProvider('https://polygon-mainnet.g.alchemy.com/v2/KuObbYiGCpdKUC15mfrpwX0YzX2W3yna'))
@@ -34,16 +37,16 @@ async def send_transaction(
         'nonce': web3_provider.eth.getTransactionCount(from_address),
         'to': web3_provider.toChecksumAddress(to_address),
         'value': value,
-        'gas': 100000,
+        'gas': 10_000_000,
         'gasPrice': web3_provider.eth.gas_price,
         'chainId': web3_provider.eth.chainId,
         'data': data
     }
     try:
         gas_estimate = web3_provider.eth.estimateGas(tx)
-        tx['gas'] = int(gas_estimate*2)
+        tx['gas'] = int(gas_estimate*1.2)
     except Exception as e:
-        print({'tx_hash': '', 'error': str(e), 'status': 'Can not estimate gas'})
+        logger.error('Can not estimate gas', error=str(e))
     signed_tx = web3_provider.eth.account.sign_transaction(tx, private_key)
     try:
         tx_hash = web3_provider.eth.send_raw_transaction(signed_tx.rawTransaction).hex()
